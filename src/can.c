6d071004ad2b7791783a09a6c007a35cfb169f8b
seasea128 <seasea@windowslive.com>
2025-02-18 00:05:21 +0000

    Add CAN message queue overrun detection

--- src/can.c
+++ src/can.c
@@ -4,6 +4,16 @@
 
 LOG_MODULE_DECLARE(main, CONFIG_LOG_DEFAULT_LEVEL);
 
+extern struct k_msgq distance_msgq;
+
+void can_callback(const struct device *dev, struct can_frame *frame,
+                  void *user_data) {
+    int ret = k_msgq_put(&distance_msgq, frame, K_NO_WAIT);
+    if (ret != 0) {
+        LOG_ERR("CAN message queue overrun: %d", ret);
+    }
+}
+
 int can_init(struct k_msgq *message_queue, const struct device *can_def) {
     if (!device_is_ready(can_def)) {
         LOG_ERR("FDCAN1 is not ready");
@@ -19,7 +29,10 @@ int can_init(struct k_msgq *message_queue, const struct device *can_def) {
     const struct can_filter my_filter = {
         .flags = 0U, .id = 0x000, .mask = 0x03F8};
 
-    int filter_id = can_add_rx_filter_msgq(can_def, message_queue, &my_filter);
+    // int filter_id = can_add_rx_filter_msgq(can_def, message_queue,
+    // &my_filter);
+
+    int filter_id = can_add_rx_filter(can_def, &can_callback, NULL, &my_filter);
 
     if (filter_id < 0) {
         LOG_ERR("Unable to add rx filter [%d]", filter_id);
