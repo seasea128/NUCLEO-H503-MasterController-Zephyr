5d73bf67de8e3620506854c1854599d2b91e79e9
seasea128 <seasea@windowslive.com>
2025-02-21 13:54:16 +0000

    Use packet that contains header instead

--- src/save_data_thread.c
+++ src/save_data_thread.c
@@ -15,16 +15,18 @@ LOG_MODULE_REGISTER(save_data_thread, LOG_LEVEL_WRN);
 #define DISK_MOUNT_PT "/" DISK_DRIVE_NAME ":"
 
 extern struct k_fifo save_data_fifo;
-static controllerMessage_DataPoints dataPoints =
-    controllerMessage_DataPoints_init_zero;
 
 void write_data_points(controllerMessage_DataPoints *dataPoints,
                        struct fs_file_t *file, struct k_msgq *output_msgq) {
-    uint8_t buffer[controllerMessage_DataPoints_size] = {0};
-
+    controllerMessage_Packet packet = {
+        .type = controllerMessage_PacketType_DATA,
+        .has_data = true,
+        .data = *dataPoints,
+        .has_session = false,
+        .session = controllerMessage_Session_init_zero};
+    uint8_t buffer[controllerMessage_Packet_size] = {0};
     pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-    bool result =
-        pb_encode(&stream, &controllerMessage_DataPoints_msg, dataPoints);
+    bool result = pb_encode(&stream, &controllerMessage_Packet_msg, &packet);
     if (!result) {
         LOG_ERR("Cannot create protobuf message: %s", PB_GET_ERROR(&stream));
         return;
@@ -45,8 +47,8 @@ void write_data_points(controllerMessage_DataPoints *dataPoints,
 
     base64_out[++base64_len] = '\n';
 
-    LOG_INF("Data point packet: %.*s", strlen(base64_out), base64_out);
-    LOG_HEXDUMP_INF(base64_out, strlen(base64_out), "Packet in hex");
+    LOG_INF("Data point packet: %.*s", base64_len, base64_out);
+    LOG_HEXDUMP_INF(base64_out, base64_len, "Packet in hex");
 
     // Write to SD card
     // ret = fs_seek(file, 0, FS_SEEK_END);
@@ -62,7 +64,10 @@ void write_data_points(controllerMessage_DataPoints *dataPoints,
     }
 
     if (output_msgq != NULL) {
-        k_msgq_put(output_msgq, base64_out, K_NO_WAIT);
+        int msgq_result = k_msgq_put(output_msgq, base64_out, K_NO_WAIT);
+        if (msgq_result != 0) {
+            LOG_ERR("Cannot put message into queue: %d", msgq_result);
+        }
     }
 
     ret = fs_sync(file);
@@ -73,15 +78,23 @@ void write_data_points(controllerMessage_DataPoints *dataPoints,
 
 cleanup:
     // Clear struct
-    memset(&dataPoints->measurement, 0, sizeof(dataPoints->measurement));
+    memset(&dataPoints->measurement, 0,
+           sizeof(controllerMessage_Measurement) *
+               sizeof(dataPoints->measurement));
     dataPoints->measurement_count = 0;
 }
 
 void write_session(controllerMessage_Session *session, struct fs_file_t *file,
                    struct k_msgq *output_msgq) {
-    uint8_t buffer[controllerMessage_DataPoints_size] = {0};
+    controllerMessage_Packet packet = {
+        .type = controllerMessage_PacketType_SESSION,
+        .has_data = false,
+        .data = controllerMessage_DataPoints_init_zero,
+        .has_session = true,
+        .session = *session};
+    uint8_t buffer[controllerMessage_Packet_size] = {0};
     pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-    bool result = pb_encode(&stream, &controllerMessage_Session_msg, session);
+    bool result = pb_encode(&stream, &controllerMessage_Packet_msg, &packet);
     if (!result) {
         LOG_ERR("Cannot create protobuf message: %s", PB_GET_ERROR(&stream));
         return;
@@ -105,8 +118,8 @@ void write_session(controllerMessage_Session *session, struct fs_file_t *file,
 
     base64_out[++base64_len] = '\n';
 
-    LOG_INF("Session packet: %.*s", strlen(base64_out), base64_out);
-    LOG_HEXDUMP_INF(base64_out, strlen(base64_out), "Packet in hex");
+    LOG_WRN("Session packet: %.*s", base64_len, base64_out);
+    LOG_HEXDUMP_INF(base64_out, base64_len, "Packet in hex");
 
     // Write to SD card
     // ret = fs_seek(file, 0, FS_SEEK_END);
@@ -122,7 +135,10 @@ void write_session(controllerMessage_Session *session, struct fs_file_t *file,
     }
 
     if (output_msgq != NULL) {
-        k_msgq_put(output_msgq, base64_out, K_NO_WAIT);
+        int msgq_result = k_msgq_put(output_msgq, base64_out, K_NO_WAIT);
+        if (msgq_result != 0) {
+            LOG_ERR("Cannot put message into queue: %d", msgq_result);
+        }
     }
 
     ret = fs_sync(file);
@@ -132,70 +148,71 @@ void write_session(controllerMessage_Session *session, struct fs_file_t *file,
     }
 }
 
-void save_data_thread() {
-    LOG_INF("Save Data Thread started");
-
-    struct fs_file_t file;
-    fs_file_t_init(&file);
-    // message msg;
-    controllerMessage_Session session = controllerMessage_Session_init_zero;
-    strncpy(session.controller_id, CONTROLLER_NAME,
-            sizeof(session.controller_id));
-
-    while (true) {
-        message msg = *(message *)k_fifo_get(&save_data_fifo, K_FOREVER);
-
-        switch (msg.msg_type) {
-        case message_measurement: {
-            // LOG_INF("Measurement data received");
-            dataPoints.measurement[dataPoints.measurement_count++] =
-                msg.data.measurement;
-            if (dataPoints.measurement_count >= 10) {
-                // LOG_INF("Enough message logged, saving to disk");
-                if (strlen(dataPoints.controller_id) == 0) {
-                    strncpy(dataPoints.controller_id, CONTROLLER_NAME,
-                            sizeof(dataPoints.controller_id));
-                }
-                write_data_points(&dataPoints, &file, NULL);
-            }
-            break;
-        }
-        case message_new_session: {
-            LOG_INF("New session message received");
-
-            if (dataPoints.measurement_count != 0) {
-                write_data_points(&dataPoints, &file, NULL);
-            }
-
-            char file_name[128] = {0};
-            snprintf(file_name, sizeof(file_name), "%s-%d.txt", CONTROLLER_NAME,
-                     msg.data.session_id);
-
-            if (file.filep != NULL) {
-                LOG_INF("File is not null");
-                session.isActive = false;
-                // TODO: Add timestamp
-                session.timestamp.nanos = 0;
-                session.timestamp.seconds = 0;
-                write_session(&session, &file, NULL);
-                file_op_close_file(&file);
-            }
-
-            session.session_id = msg.data.session_id;
-            session.isActive = true;
-            // TODO: Add timestamp
-            session.timestamp.nanos = 0;
-            session.timestamp.seconds = 0;
-
-            file_op_open_file(&file, file_name, FS_O_APPEND);
-            LOG_INF("Opened new file");
-            write_session(&session, &file, NULL);
-            break;
-        }
-        case message_undefined: {
-            LOG_ERR("Undefined message received");
-            break;
-        }
-        }
-    }
-}
+// void save_data_thread() {
+//     LOG_INF("Save Data Thread started");
+//
+//     struct fs_file_t file;
+//     fs_file_t_init(&file);
+//     // message msg;
+//     controllerMessage_Session session = controllerMessage_Session_init_zero;
+//     strncpy(session.controller_id, CONTROLLER_NAME,
+//             sizeof(session.controller_id));
+//
+//     while (true) {
+//         message msg = *(message *)k_fifo_get(&save_data_fifo, K_FOREVER);
+//
+//         switch (msg.msg_type) {
+//         case message_measurement: {
+//             // LOG_INF("Measurement data received");
+//             dataPoints.measurement[dataPoints.measurement_count++] =
+//                 msg.data.measurement;
+//             if (dataPoints.measurement_count >= 10) {
+//                 // LOG_INF("Enough message logged, saving to disk");
+//                 if (strlen(dataPoints.controller_id) == 0) {
+//                     strncpy(dataPoints.controller_id, CONTROLLER_NAME,
+//                             sizeof(dataPoints.controller_id));
+//                 }
+//                 write_data_points(&dataPoints, &file, NULL);
+//             }
+//             break;
+//         }
+//         case message_new_session: {
+//             LOG_INF("New session message received");
+//
+//             if (dataPoints.measurement_count != 0) {
+//                 write_data_points(&dataPoints, &file, NULL);
+//             }
+//
+//             char file_name[128] = {0};
+//             snprintf(file_name, sizeof(file_name), "%s-%d.txt",
+//             CONTROLLER_NAME,
+//                      msg.data.session_id);
+//
+//             if (file.filep != NULL) {
+//                 LOG_INF("File is not null");
+//                 session.isActive = false;
+//                 // TODO: Add timestamp
+//                 session.timestamp.nanos = 0;
+//                 session.timestamp.seconds = 0;
+//                 write_session(&session, &file, NULL);
+//                 file_op_close_file(&file);
+//             }
+//
+//             session.session_id = msg.data.session_id;
+//             session.isActive = true;
+//             // TODO: Add timestamp
+//             session.timestamp.nanos = 0;
+//             session.timestamp.seconds = 0;
+//
+//             file_op_open_file(&file, file_name, FS_O_APPEND);
+//             LOG_INF("Opened new file");
+//             write_session(&session, &file, NULL);
+//             break;
+//         }
+//         case message_undefined: {
+//             LOG_ERR("Undefined message received");
+//             break;
+//         }
+//         }
+//     }
+// }
